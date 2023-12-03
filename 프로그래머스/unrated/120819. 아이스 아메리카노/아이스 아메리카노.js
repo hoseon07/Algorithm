e93cf8761b9ee2770ae19d716c5d15ca05269944
hoseon07 <130675352+hoseon07@users.noreply.github.com>
2023-12-03 11:24:06 +0900

    [level unrated] Title: 아이스 아메리카노, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120819. 아이스 아메리카노/아이스 아메리카노.js
@@ -0,0 +1,9 @@
+function solution(money) {
+    const americano = 5500;
+    
+    const glass = Math.floor(money / americano);
+    const change = money % americano;
+    
+    const ans = [glass, change];
+    return ans;
+}
