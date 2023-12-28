03364a55bda0aae07ad6d527097056fee28e86ca
hoseon07 <130675352+hoseon07@users.noreply.github.com>
2023-12-28 23:22:54 +0900

    [level unrated] Title: 부분 문자열 이어 붙여 문자열 만들기, Time: 0.46 ms, Memory: 34 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181911. 부분 문자열 이어 붙여 문자열 만들기/부분 문자열 이어 붙여 문자열 만들기.js
@@ -0,0 +1,10 @@
+function solution(my_strings, parts) {
+    var answer = '';
+    for(let i=0; i<my_strings.length;i++){
+        answer+= my_strings[i].split('').slice(parts[i][0],parts[i][1]+1).join('')
+    }
+    return answer;
+}
+
+console.log(solution(["progressive", "hamburger", "hammer", "ahocorasick"],
+[[0, 4], [1, 2], [3, 5], [7, 7]]));
