6a70e61f97c602c2cd4901c01caec3bd4cfd4905
hoseon07 <130675352+hoseon07@users.noreply.github.com>
2024-01-10 21:25:27 +0900

    [level unrated] Title: 1로 만들기, Time: 0.11 ms, Memory: 33 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181880. 1로 만들기/1로 만들기.js
@@ -0,0 +1,25 @@
+const n = [10, 12, 17, 2, 7];
+
+function solution(n) {
+  let cnt = 0;
+  
+  for(el of n) {
+    let num = el;
+
+    while (num !== 1) {
+      if (num % 2 === 0) {
+        num /= 2;
+        
+      } else {
+        num -= 1;
+        num /= 2;
+      }
+
+      cnt++;
+    }
+  }
+
+  return cnt;
+}
+
+solution(n)
