a28653c8a523e3dba144dd17e02b02c5af071a32
hoseon07 <130675352+hoseon07@users.noreply.github.com>
2024-02-08 22:05:03 +0900

    [level 0] Title: 주사위 게임 1, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.js
+++ 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.js
@@ -1,6 +1,4 @@
 function solution(a, b) {
-    // 홀수 + 짝수 = 짝수
-    if(a%2 === 1 && b % 2 === 1) return a**2 + b**2
-    if(a%2 === 0 && b % 2 === 0) return Math.abs(a-b)
-    return 2*(a+b)
+    var answer = 0;
+    return answer;
 }
