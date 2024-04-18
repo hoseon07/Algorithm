562733939a0f38db5fcc7734ea453b6938a4828a
seon <130675352+hoseon07@users.noreply.github.com>
2024-04-18 21:50:40 +0900

    [level 2] Title: 택배상자, Time: 125.46 ms, Memory: 75.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/131704. 택배상자/택배상자.js
@@ -0,0 +1,19 @@
+function solution(order) {
+    var answer = 0;
+    return answer;
+}function solution(order) {
+  let result = 0;
+  const stack = [];
+
+  for (let i = 1; i <= order.length; i++) {
+    stack.push(i);
+
+    
+    while (stack.length !== 0 && stack.at(-1) === order[result]) {
+      stack.pop();
+      result++;
+    }
+  }
+
+  return result;
+}
